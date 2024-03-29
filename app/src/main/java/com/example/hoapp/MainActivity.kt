7ba6620ca2b68f89679f145ceb4811b2b37f856e
Rey <reygregmont@gmail.com>
2024-03-29 12:35:25 -0600

    Inicio del proyecto con barra de navegacion entre fragments

---
+++ app/src/main/java/com/example/hoapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.hoapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.hoapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityMainBinding
+
+    private  lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+    private fun initNavigation(){
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/hoapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,26 @@
+package com.example.hoapp.ui.horoscope
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hoapp.R
+import com.example.hoapp.databinding.FragmentHoroscopeBinding
+
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding:FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
